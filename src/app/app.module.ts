d8238608d7bdbdb300cd1338f09faf8368799ab6
aaronshivers <aaron@moonmail.org>
2020-04-08 16:30:24 -0500

    setup header for mobile

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './heading/header.component';
+import { SelectorsComponent } from './selectors/selectors.component';
+import { CountriesComponent } from './countries/countries.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    SelectorsComponent,
+    CountriesComponent
   ],
   imports: [
     BrowserModule,
